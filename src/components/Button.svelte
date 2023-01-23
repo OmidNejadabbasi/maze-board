564ca5004796d59d8dae2c5067de862e24db6dfa
Omid Nejadabbasi <nejadabbasio@gmail.com>
2023-01-23 21:10:03 +0330

    generate maze with backtracing algorithm

---
+++ src/components/Button.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { cssVariables } from "../utils";
+  export let color = "grey";
+</script>
+
+<button
+  on:click
+  class="p-3 border rounded-md text-white"
+  use:cssVariables={{ color }}
+>
+  <slot />
+</button>
+
+<style>
+  button {
+    background-color: var(--color);
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,19 @@
 <script>
   import "../app.css";
   import Board from "../components/Board.svelte";
+  import Button from "../components/Button.svelte";
 </script>
 
-<div class="flex justify-center">
+<div class="flex flex-col items-center">
+  <div class="flex flex-col ">
+    <p class="text-center">Generate Maze with:</p>
+    <div class="h-3" />
+    <div class="flex">
+      <Button color="#e32423">Back tracing</Button>
+      <Button color="#242e99">Back tracing</Button>
+      <Button color="#23e324">Back tracing</Button>
+    </div>
+  </div>
+  <div class="h-12" />
   <Board />
 </div>
